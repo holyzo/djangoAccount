b19d55fb6fa9ca2549d88e2327c3687c1d1f4c13
jhee.noh <jhee.noh@lgepartner.com>
2022-07-27 16:07:47 +0900

    move templates dir

--- ably/settings.py
+++ ably/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'ably.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'ably/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
