bc7a61025f79bb7005e7d249f27e434cd99d0116
holyzo <holyzo@naver.com>
2022-07-27 23:45:59 +0900

    전번인증 템플릿 벨리데이션 에러 표시 부분 작업1

--- cert/forms.py
+++ cert/forms.py
@@ -1,30 +1,51 @@
 from django import forms
-from django.core.validators import RegexValidator
-from django.db import models
-from ably import common
-
-phone_validator = RegexValidator(common.PATT_PHONE,
-                                 "The phone number must be provided only number letter")
-
-ssNumberPre_validator = RegexValidator(common.PATT_SOCIAL_SECURITY_NUMBER_PRE,
-                                 "The social security number prefix must be provided only 6 numbers letter")
+from .models import CertPhoneInput
+import pdb
 
 
-class CertPhoneInputForm(forms.Form):
-    name                        = models.CharField(max_length=128)
-    socialSecurityNumberPre   = models.CharField(max_length=6,
+
+class CertPhoneInputForm(forms.ModelForm):
+    certType = 0
+
+    class Meta:
+        model = CertPhoneInput
+        fields = ['name', 'socialSecurityNumberPre', 'gender', 'phone']
+
+    '''
+    name                        = models.CharField('name', max_length=128,
+                                                    validators=[MinValueValidator(2),])
+    socialSecurityNumberPre     = models.CharField('socialSecurityNumber',max_length=6,
                                                       validators=[ssNumberPre_validator,])
-    gender                      = models.CharField(max_length=1)
-    phone                       = models.CharField(max_length=11,
+    gender                      = models.CharField('gender', max_length=1)
+    phone                       = models.CharField('phone', max_length=11,
                                                    validators=[phone_validator,])
     certType = 0
 
-class CertPhoneRecvNumberForm(forms.Form):
+    def clean_name(self):
+        pdb.set_trace()
+
+        return name
+
+    def clean(self):
+        clean_name = self.cleaned_data.get('name')
+        if not clean_name:
+            msg = 'The name must be provided 2-128 size letter'
+            self.add_error('name', msg)
+            raise forms.ValidationError(msg)
+
+        #clean_socialSecurityNumberPre = self.cleaned_data.get('socialSecurityNumberPre')
+    '''
+
+
+
+
+class CertPhoneRecvNumberForm(forms.ModelForm):
     certType = 0
     name = ''
     phone = ''
-
+'''
     certNum = models.CharField(max_length=6,
                               validators=[
                                 RegexValidator("[0-9]{6}", "The cert number must be provided only number letter"),
                               ])
+'''
